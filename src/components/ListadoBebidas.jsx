e5a7b1ce027f466d9bc874a2c73041b69e5f3b7c
JesusSalazar1997 <epastrabajando@gmail.com>
2022-07-13 21:03:59 -0500

    Se carga el proyecto

---
+++ src/components/ListadoBebidas.jsx
@@ -0,0 +1,16 @@
+import useBebidas from "../hooks/useBebidas";
+import { Row } from "react-bootstrap";
+import Bebidas from "./Bebidas";
+const ListadoBebidas = () => {
+  const { bebidas } = useBebidas();
+
+  return (
+    <Row className="mt-5">
+      {bebidas.map((bebida) => (
+        <Bebidas key={bebida.idDrink} bebida={bebida} />
+      ))}
+    </Row>
+  );
+};
+
+export default ListadoBebidas;
